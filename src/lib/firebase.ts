738c1a38069dfa29c688f41debc179fc540cadad
Ramone 2.0 <ramawan01@gmail.com>
2025-10-26 07:14:07 +0000

    saya sudah memasukan sdk informasi ini : "// Import the functions you ne

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBz85qJO_bOY00zjYr6BfNNzLz3aMnTcFo",
+  authDomain: "migration-earth-project.firebaseapp.com",
+  projectId: "migration-earth-project",
+  storageBucket: "migration-earth-project.firebasestorage.app",
+  messagingSenderId: "1000322641359",
+  appId: "1:1000322641359:web:e89d009b81e187270e36ba"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
